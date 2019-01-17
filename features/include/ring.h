1429a99862323fd6e5914470f96b3a461efa62c4
karan <karanpatelkrn@gmail.com>
2019-01-16 17:55:30 -0800

    making buffer class iterable

--- features/include/ring.h
+++ features/include/ring.h
@@ -5,12 +5,16 @@
 
 template <class T>
 class ring {
+
 private:
     int pos_;
     int size_;
     T *array_ptr_;
+
 public:
     class iterator;
+
+public:
 
     ring(int size) : pos_(0), size_(size), array_ptr_(NULL){
         array_ptr_ = new T[size_];
@@ -31,6 +35,14 @@ public:
         return array_ptr_[index];
     }
 
+    iterator begin() {
+        return iterator(0, *this);
+    }
+
+    iterator end() {
+        return iterator(size_, *this);
+    }
+
     ~ring() {
         delete[] array_ptr_;
     }
@@ -38,8 +50,31 @@ public:
 
 template <class T>
 class ring<T>::iterator {
+
+private:
+    int pose_;
+    ring &aRing_;
+
 public:
-    iterator() {
+    iterator(int pos, ring& aRing) : pose_(pos), aRing_(aRing) {
+    }
+
+    iterator &operator ++(int) {
+        pose_++;
+        return *this;
+    }
+
+    iterator &operator ++() {
+        pose_++;
+        return *this;
+    }
+
+    bool operator !=(const iterator &other) const {
+        return pose_ != other.pose_;
+    }
+
+    T &operator *() {
+        return aRing_.get(pose_);
     }
 };
 
--- features/src/nested.cpp
+++ features/src/nested.cpp
@@ -3,8 +3,6 @@
 
 int main() {
 
-    ring<std::string>::iterator it;
-
     ring<std::string> textring(3);
 
     textring.add("one");
@@ -15,6 +13,18 @@ int main() {
 
     for(int i = 0; i < textring.size(); i++) {
         std::cout << textring.get(i) << std::endl;
+    }
+
+    std::cout << "=========" << std::endl;
+
+    for(ring<std::string>::iterator it = textring.begin(); it != textring.end(); it++) {
+        std::cout << *it << std::endl;
+    }
+
+    std::cout << "=========" << std::endl;
+
+    for(std::string text : textring) {
+        std::cout << text << std::endl;
     }
 
     return 0;
