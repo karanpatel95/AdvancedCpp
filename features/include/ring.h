2039b3246a80f962587260337b489deb3a24496a
karan <karanpatelkrn@gmail.com>
2019-01-16 16:53:25 -0800

    working ring buffer

--- features/include/ring.h
+++ features/include/ring.h
@@ -5,15 +5,41 @@
 
 template <class T>
 class ring {
-  public:
+private:
+    int pos_;
+    int size_;
+    T *array_ptr_;
+public:
     class iterator;
+
+    ring(int size) : pos_(0), size_(size), array_ptr_(NULL){
+        array_ptr_ = new T[size_];
+    }
+
+    int size() {
+        return size_;
+    }
+
+    void add(T value) {
+        array_ptr_[pos_++] = value;
+        if(pos_ == size_) {
+            pos_ = 0;
+        }
+    }
+
+    T &get(int index) {
+        return array_ptr_[index];
+    }
+
+    ~ring() {
+        delete[] array_ptr_;
+    }
 };
 
 template <class T>
 class ring<T>::iterator {
 public:
     iterator() {
-        std::cout << "this is iterator" << std::endl;
     }
 };
 
--- features/src/nested.cpp
+++ features/src/nested.cpp
@@ -5,19 +5,17 @@ int main() {
 
     ring<std::string>::iterator it;
 
-/*
     ring<std::string> textring(3);
 
     textring.add("one");
     textring.add("two");
     textring.add("three");
     textring.add("four");
-
-    std::cout << textring.size() << std::endl;
+    textring.add("five");
 
     for(int i = 0; i < textring.size(); i++) {
         std::cout << textring.get(i) << std::endl;
     }
-*/
+
     return 0;
 }
