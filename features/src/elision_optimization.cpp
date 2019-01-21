aaa3c80b393207910bcb501cc44dbb8f9fbc357e
karan <karanpatelkrn@gmail.com>
2019-01-21 12:42:23 -0800

    elision optimization

---
+++ features/src/elision_optimization.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+class Test {
+public:
+    Test() {
+        std::cout << "constructor without parameter" << std::endl;
+    }
+
+    Test(std::string text) {
+        std::cout << "parameterized constructor" << std::endl;
+    }
+
+    Test(const Test &other) {
+        std::cout << "copy constructor" << std::endl;
+    }
+
+    ~Test() {
+        std::cout << "destructor" << std::endl;
+    }
+
+    void operator =(const Test &other) {
+        std::cout << " = operator" << std::endl;
+    }
+
+    friend std::ostream &operator <<(std::ostream &out, const Test &other);
+};
+
+std::ostream &operator <<(std::ostream &out, const Test &other) {
+    out << "hello Test class";
+    return out;
+}
+
+Test getTest() {
+    return Test();
+}
+
+int main() {
+    Test test = getTest();
+    std::cout << test << std::endl;
+    return 0;
+}
